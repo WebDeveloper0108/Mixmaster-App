b0a2f67e15f8908700bc3eef166581100661d430
WebDeveloper0108 <masterofwebdev@gmail.com>
2024-10-20 13:25:00 +0530

    files

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+import Wrapper from "../assets/wrappers/AboutPage";
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h1>About Us</h1>
+      <p>
+        Introducing "MixMaster", the ultimate party sidekick app that fetches
+        cocktails from the hilarious Cocktails DB API. With a flick of your
+        finger, you'll unlock a treasure trove of enchanting drink recipes
+        that'll make your taste buds dance and your friends jump with joy. Get
+        ready to shake up your mixology game, one fantastical mocktail at a
+        time, and let the laughter and giggles flow!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus nemo
+        iste, temporibus laborum voluptates dolore sit. Quidem pariatur
+        obcaecati eligendi distinctio, qui nam, ipsum dolore consequatur debitis
+        soluta quam doloribus quo perspiciatis, odio modi ipsam officia
+        similique iste recusandae harum corporis ratione. Voluptatem laudantium
+        nesciunt aspernatur hic voluptatum dicta reiciendis mollitia distinctio,
+        repellendus ducimus expedita esse laborum blanditiis quibusdam
+        perspiciatis?
+      </p>
+    </Wrapper>
+  );
+};
+
+export default About;
